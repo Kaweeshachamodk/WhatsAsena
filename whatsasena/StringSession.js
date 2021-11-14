17b2044c76d3cf41cb755010e80501ff13c8a837
FIRUS _FUCKER <88616416+Kaweeshachamodk@users.noreply.github.com>
2021-11-14 12:12:01 +0530

    Update StringSession.js

--- whatsasena/StringSession.js
+++ whatsasena/StringSession.js
@@ -28,7 +28,7 @@ class StringSession {
     }
 
     createStringSession(dict) {
-        return 'WHITE;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'STEFANIE===' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 
